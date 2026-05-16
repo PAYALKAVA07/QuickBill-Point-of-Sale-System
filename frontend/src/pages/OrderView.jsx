9508094f4253a084f58a59e880e63aef7ef8aeb5
PAYALKAVA07 <payalkava9@gmail.com>
2026-05-16 13:40:34 +0530

    orderview.jsx updated

--- frontend/src/pages/OrderView.jsx
+++ frontend/src/pages/OrderView.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import html2canvas from 'html2canvas'
-import jsPDF from 'jsPDF'
+// import jsPDF from 'jsPDF'
+import jsPDF from "jspdf";
 
 export default function OrderView(){
   const { id } = useParams();
